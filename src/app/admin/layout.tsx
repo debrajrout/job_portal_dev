5e0a3c497127f7b0cebd52d64d08c469012a9342
debrajrout <debrajrout28@gmail.com>
2024-01-23 23:32:36 +0530

    Add seed script and postinstall command

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Metadata } from "next";
-import AdminNavbar from "./AdminNavbar.jsx";
+import AdminNavbar from "./AdminNavbar";
 
 export const metadata: Metadata = {
   title: "Admin",
